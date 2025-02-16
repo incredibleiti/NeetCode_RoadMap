93f1390ee0c83bd22e875924bd77fcff3e4d4d2a
Iti Tyagi <intechworx@gmail.com>
2025-02-16 14:19:22 +0100

    Create ValidAnagram.cpp

---
+++ ArraysAndHashing/ValidAnagram.cpp
@@ -0,0 +1,27 @@
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+// Example 1:
+
+// Input: s = "racecar", t = "carrace"
+
+// Output: true
+// Example 2:
+
+// Input: s = "jar", t = "jam"
+
+// Output: false
+// Constraints:
+
+// s and t consist of lowercase English letters.
+
+/////////////////////////Brut Force///////////////////////////////
+
+bool isAnagram(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(),t.end());
+    if(s==t) {
+        return true;
+    }  else {
+        return false;
+    }    
+}
